bd884de90d5b739fc7e7c4a7fc28663d2b897a72
ayuashu <54568763+ayuashu@users.noreply.github.com>
2023-12-17 16:11:35 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
-
+import { User, addDoc } from './config.js';
 const app = express();
 
+app.use(express.json());
+
+app.post('/create', async (req, res) => {
+  const data=req.body;
+  await addDoc(User,data);
+  res.send({msg: "User Created"});
+})
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    });
+  console.log('Server is running on port 3000');
+});
+
